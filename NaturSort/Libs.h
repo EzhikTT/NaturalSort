142aac4d6b79f5aa1ff82a45cc55b427d8165455
EzhikTT <sprf8611@gmail.com>
2015-07-16 21:08:36 +0300

    Add enum for groups sorting

--- NaturSort/Libs.h
+++ NaturSort/Libs.h
@@ -9,3 +9,28 @@ enum typeDate
 	email,
 	other,
 };
+
+enum protocolType
+{
+	dhcp,
+	dns,
+	ftp,
+	http,
+	https,
+	ntp,
+	nntp,
+	ssh,
+	telnet,
+};
+
+enum fEtype
+{
+	grafics,
+	sound,
+	video,
+	text,
+	webpage,
+	exefile,
+	refenceCodeFile,
+	objFile,
+};
